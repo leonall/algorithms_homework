3512956e046f0dcdcdc9ad92061019d18b7a80ef
leonall <leon_hw@163.com>
2018-06-13 14:41:16 +0800

    调整代码格式

--- search/first_occurance.py
+++ search/first_occurance.py
@@ -41,31 +41,31 @@ def first_occurance2(ls, query):
     else:
         return False
 
-    class Test_bubble_sort(unittest.TestCase):
+class Test_bubble_sort(unittest.TestCase):
 
-        def setUp(self):
-            global first_occurance, first_occurance2
-            self.fun1 = first_occurance
-            self.fun2 = first_occurance2
+    def setUp(self):
+        global first_occurance, first_occurance2
+        self.fun1 = first_occurance
+        self.fun2 = first_occurance2
 
-        def _test_binary_search(self, first_occurance):
-            for _ in range(10):
-                arr = sorted(np.random.randint(0, 10, 20).tolist())  # maybe duplicated number,
-                var = np.random.choice(range(10))
-                if var in arr:
-                    self.assertEqual(first_occurance(arr, var), arr.index(var))
-                else:
-                    self.assertFalse(binary_search(arr, var), False)
-                arr2 = [1] * 10
-                self.assertEqual(first_occurance(arr2, 1), 0)
-                self.assertFalse(first_occurance(arr2, 0), False)
-                self.assertFalse(first_occurance(arr2, 2.5), False)
+    def _test_binary_search(self, first_occurance):
+        for _ in range(10):
+            arr = sorted(np.random.randint(0, 10, 20).tolist())  # maybe duplicated number,
+            var = np.random.choice(range(10))
+            if var in arr:
+                self.assertEqual(first_occurance(arr, var), arr.index(var))
+            else:
+                self.assertFalse(binary_search(arr, var), False)
+            arr2 = [1] * 10
+            self.assertEqual(first_occurance(arr2, 1), 0)
+            self.assertFalse(first_occurance(arr2, 0), False)
+            self.assertFalse(first_occurance(arr2, 2.5), False)
 
-        def test_binary_search(self):
-            self._test_binary_search(self.fun1)
+    def test_binary_search(self):
+        self._test_binary_search(self.fun1)
 
-        def test_binary_search2(self):
-            self._test_binary_search(self.fun2)
+    def test_binary_search2(self):
+        self._test_binary_search(self.fun2)
 
 
 if __name__ == '__main__':
